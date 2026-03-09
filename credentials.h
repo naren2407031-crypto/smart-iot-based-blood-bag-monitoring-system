d5119821ae08fe08ab450822d40e40e2faff7a77
naren2407031-crypto <naren.2407031@srec.ac.in>
2026-03-09 19:39:25 +0530

    add Blynk and WiFi credentials

---
+++ credentials.h
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/YOUR_USERNAME/blood-bag-monitor.git
+#define BLYNK_TEMPLATE_ID   "Template ID"
+#define BLYNK_TEMPLATE_NAME "Template Name"
+#define BLYNK_AUTH_TOKEN    "Auth token"
+
+char ssid[] = "ssid";
+char pass[] = "password";
